20b009272c76519dddaf4acce1db343fa92bd02e
GitHub Action <action@github.com>
2025-11-10 19:19:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/centronias/tiner/services/MyProjectService.kt
+++ src/main/kotlin/com/github/centronias/tiner/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.centronias.tiner.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.centronias.tiner.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
